7a7744b6309872c9757bfa3e4ea08176b9ff2101
Popcorn-0924 <0924popcorn0924@gmail.com>
2025-01-12 23:55:40 +0800

    0109

--- src/input/AppStore.js
+++ src/input/AppStore.js
@@ -1,17 +1,13 @@
 import { makeAutoObservable } from 'mobx';
 
-class AppStore {
-    inputText = '';  // 儲存 Input 中的文字
-    tableData = [];  // 儲存 Table 的資料
-
-    constructor() {
-        makeAutoObservable(this); // 使這些屬性和方法成為 MobX 的 observable 和 action
-    }
+export const appStore = makeAutoObservable({
+    inputText: '',  // 儲存 Input 中的文字
+    tableData: [],  // 儲存 Table 的資料
 
     // 更新 inputText
     updateInputText(newText) {
         this.inputText = newText;
-    }
+    },
 
     // 新增資料到 table
     addToTable() {
@@ -19,7 +15,7 @@ class AppStore {
             this.tableData.push(this.inputText);
             this.inputText = ''; // 清空 Input
         }
-    }
+    },
 
     // 更新 Table 中特定的資料
     updateTableText(index, newText) {
@@ -27,6 +23,4 @@ class AppStore {
             this.tableData[index] = newText;
         }
     }
-}
-
-export const appStore = new AppStore();
+});
